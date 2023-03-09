aa095ee3e7057fae98949ebf5bce07b291c8adec
Jonathan Herman <jdh11235@gmail.com>
2023-03-08 23:32:01 -0600

    Make sorter .txt instead of .csv; add instructions

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 1. Make sure [Node JS](https://nodejs.org/en/) is installed
 2. Run `npm install`
-3. Run `node whateverScript.js`
+3. Read the comments at the top of a script to see what input files it expects you to create
+4. Run `node whateverScript.js`
--- sortByNumberOfLetters.js
+++ sortByNumberOfLetters.js
@@ -1,58 +1,37 @@
+// input: you must create an input.txt file
+// with each piece of text you want sorted on a new line
+
+// output: script will create an output.txt
+
 const fs = require('fs');
-const csv = require('csv-parser');
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-const inputFile = 'input.csv';
-const outputFile = 'output.csv';
+const inputFile = 'input.txt';
+const outputFile = 'output.txt';
 
-// Define the CSV writer configuration
-const csvWriter = createCsvWriter({
-    path: outputFile,
-    header: [
-        { id: 'column', title: 'Column' }
-    ]
-});
+// Read the input file and sort the words
+fs.readFile(inputFile, 'utf-8', (error, data) => {
+    if (error) {
+        console.error(`Error reading input file: ${error}`);
+        return;
+    }
 
-// Read the CSV file and sort the data
-const data = [];
-fs.createReadStream(inputFile)
-    .pipe(csv({ headers: false }))
-    .on('data', (row) => {
-        data.push(row[0]);
-    })
-    .on('end', () => {
-        // Sort the data by the number of letters in the column
-        data.sort((a, b) => {
-            return a.length - b.length;
-        });
+    // Split the input text into an array of words
+    const words = data.trim().split('\n');
 
-        // Write the sorted data to a new CSV file without a header row
-        const records = data.map((value) => {
-            return { column: value };
-        });
-
-        csvWriter.writeRecords(records)
-            .then(() => {
-                console.log(`Successfully sorted and saved to ${outputFile}`);
-            })
-            .catch((error) => {
-                console.error(`Error writing CSV file: ${error}`);
-            });
-
-        // Remove the header row from the output file
-        fs.readFile(outputFile, 'utf-8', (error, data) => {
-            if (error) {
-                console.error(`Error reading CSV file: ${error}`);
-                return;
-            }
-
-            const updatedData = data.replace(/^.*?\n/, '');
-            fs.writeFile(outputFile, updatedData, (error) => {
-                if (error) {
-                    console.error(`Error writing CSV file: ${error}`);
-                } else {
-                    console.log(`Successfully removed header from ${outputFile}`);
-                }
-            });
-        });
+    // Sort the words by the number of letters
+    words.sort((a, b) => {
+        return a.length - b.length;
     });
+
+    // Join the sorted words into a single string
+    const outputText = words.join('\n');
+
+    // Write the sorted words to the output file
+    fs.writeFile(outputFile, outputText, (error) => {
+        if (error) {
+            console.error(`Error writing output file: ${error}`);
+        } else {
+            console.log(`Successfully sorted and saved to ${outputFile}`);
+        }
+    });
+});
