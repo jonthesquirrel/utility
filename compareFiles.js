16b6914a00a34b439bdeaa9bb20269a2892bf3d5
Jonathan Herman <jdh11235@gmail.com>
2023-03-09 01:16:28 -0600

    Deal with windows line endings and print both file

--- compareFiles.js
+++ compareFiles.js
@@ -8,14 +8,14 @@ const file1Contents = fs.readFileSync(file1Path, 'utf8');
 const file2Contents = fs.readFileSync(file2Path, 'utf8');
 
 // Split the contents of each file into arrays of lines
-const file1Lines = file1Contents.split('\n');
-const file2Lines = file2Contents.split('\n');
+const file1Lines = file1Contents.split(/\r?\n/).map(line => line.replace('\r', ''));
+const file2Lines = file2Contents.split(/\r?\n/).map(line => line.replace('\r', ''));
 
 // Loop through each line in file1 and check if it's also in file2
-let i = 0;
-for (let line of file1Lines) {
-  if (line !== file2Lines[i]) {
-    console.log(i, line);
+for (let i = 0; i < file1Lines.length; i++) {
+  const file1Line = file1Lines[i];
+  const file2Line = file2Lines[i];
+  if (file1Line !== file2Line) {
+    console.log(`${i} | ${file1Line} | ${file2Line}`);
   }
-  i++;
 }
