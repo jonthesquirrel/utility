3c14146038c8b64563859562f755a5049c182626
Jonathan Herman <jdh11235@gmail.com>
2023-05-28 21:58:18 -0500

    Add payment column, accurate start, not end

--- installment_payments.py
+++ installment_payments.py
@@ -3,32 +3,29 @@ from datetime import datetime, timedelta
 import argparse
 
 def create_csv(start_date: str, start_amount: int, increment_amount: int, end_amount: int):
-    # Convert start date string to datetime object
+    # Convert string argument to datetime object
     start_date = datetime.strptime(start_date, "%b %d %Y")
 
-    # Initialize increment values
     date_increment = timedelta(weeks=1)
 
-    # Open (and create) a CSV file
     with open('output.csv', 'w', newline='') as file:
         writer = csv.writer(file)
 
-        # Write the header
-        writer.writerow(["Date", "Total Paid", "Debt Balance"])
+        # Header
+        writer.writerow(["Date", "Total Paid", "Payment", "Debt Balance"])
 
-        # While amount is less than end_amount minus increment_amount
-        while start_amount < end_amount - increment_amount:
+        # Rows
+        while start_amount < end_amount:
+
+            # Final remainder payment
+            if start_amount + increment_amount > end_amount:
+                increment_amount = end_amount - start_amount
+
             debt_balance = end_amount - start_amount
-            # Write the row with current date, amount, and reverse amount
-            writer.writerow([start_date.strftime("%b %d %Y"), f'${start_amount:.2f}', f'${debt_balance:.2f}'])
+            writer.writerow([start_date.strftime("%b %d %Y"), f'${start_amount:.2f}', f'${increment_amount:.2f}', f'${debt_balance:.2f}'])
 
-            # Increment date and amount
             start_date += date_increment
             start_amount += increment_amount
-
-        # Add final payment
-        if start_amount < end_amount:
-            writer.writerow([start_date.strftime("%b %d %Y"), f'${end_amount:.2f}', f'$0.00'])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Create a CSV file with dates and amounts.")
