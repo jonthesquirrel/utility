9fa7e99c3738e2cd5dfa29327cc1fb6b974e4517
Jonathan Herman <jdh11235@gmail.com>
2023-05-28 21:43:12 -0500

    Add larger final payment to cover remainder

--- installment_payments.py
+++ installment_payments.py
@@ -9,10 +9,6 @@ def create_csv(start_date: str, start_amount: int, increment_amount: int, end_am
     # Initialize increment values
     date_increment = timedelta(weeks=1)
 
-    # Calculate the largest amount
-    num_increments = (end_amount - start_amount) // increment_amount
-    largest_amount = start_amount + num_increments * increment_amount
-
     # Open (and create) a CSV file
     with open('output.csv', 'w', newline='') as file:
         writer = csv.writer(file)
@@ -20,15 +16,19 @@ def create_csv(start_date: str, start_amount: int, increment_amount: int, end_am
         # Write the header
         writer.writerow(["Date", "Total Paid", "Debt Balance"])
 
-        # While amount is less than or equal to end_amount
-        while start_amount <= end_amount:
+        # While amount is less than end_amount minus increment_amount
+        while start_amount < end_amount - increment_amount:
+            debt_balance = end_amount - start_amount
             # Write the row with current date, amount, and reverse amount
-            writer.writerow([start_date.strftime("%b %d %Y"), f'${start_amount:.2f}', f'${largest_amount:.2f}'])
+            writer.writerow([start_date.strftime("%b %d %Y"), f'${start_amount:.2f}', f'${debt_balance:.2f}'])
 
-            # Increment date and amount, decrement largest_amount
+            # Increment date and amount
             start_date += date_increment
             start_amount += increment_amount
-            largest_amount -= increment_amount
+
+        # Add final payment
+        if start_amount < end_amount:
+            writer.writerow([start_date.strftime("%b %d %Y"), f'${end_amount:.2f}', f'$0.00'])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Create a CSV file with dates and amounts.")
